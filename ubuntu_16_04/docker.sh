a564b4406d27cdeb82e29425215d30911ef56d2d
demostenes <kbogus@gmail.com>
2017-03-28 14:46:51 +0200

    sklejka

--- ubuntu_16_04/docker.sh
+++ ubuntu_16_04/docker.sh
@@ -4,3 +4,11 @@ sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 sudo apt-get update
 sudo apt-get install -y docker-engine
+
+
+# sklejone razem
+sudo apt-get install -y software-properties-common python-software-properties apt-transport-https && \
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && \
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' && \
+sudo apt-get update && \
+sudo apt-get install -y docker-engine
