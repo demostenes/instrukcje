d3b238a8a5ad78ebb57570b89780c1ae01231175
demostenes <kbogus@gmail.com>
2017-05-08 18:46:00 +0200

    aaa

--- ubuntu_16_04/vuejs_dev.sh
+++ ubuntu_16_04/vuejs_dev.sh
@@ -1,8 +1,8 @@
 # instalacja z PPA na podstawie https://github.com/nodesource/distributions
-sudo apt install -y curl
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt install -y nodejs
-sudo npm install npm@latest -g
+sudo apt install -y curl && \
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash - && \
+sudo apt install -y nodejs && \
+sudo npm install npm@latest -g && \
 sudo npm install -g vue-cli
 # i tworze nowy projekt
 vue init webpack metronic-test
@@ -11,11 +11,12 @@ npm install
 npm run dev
 
 
-# instalacja w oficjalnych repozytoriow ubuntu
+
+# instalacja z oficjalnych repozytoriow ubuntu
 # raczej nie działa bo webpack wymaga nowszej wersji nodejs
 # nie jest tam dostepna
-sudo apt install -y nodejs npm
-sudo npm install -g vue-cli
+sudo apt install -y nodejs npm && \
+sudo npm install -g vue-cli && \
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 # i tworze nowy projekt
 vue init webpack metronic-test
